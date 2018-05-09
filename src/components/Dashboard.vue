7aa0bc767d77b6337cda09bbd2b93179744c464a
morgyken <morgyken@gmail.com>
2018-05-09 22:59:11 +0300

    Vue Router Boiler Plate

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="dashbard">
+        <h3> Dashboard </h3>
+        <div class="fixed-action-btn">
+         <router-link to ="/new" 
+         class ="btn-floating btn-large red">
+         <i class="material-icons">add</i>
+         </router-link>
+         </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'dashboad',
+    data() {
+        return {
+
+        }
+    }
+}
+
+
+</script>
---
+++ src/components/ViewEmployee.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="view-employee">
+        <h3>View Employee </h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'view-Employee',
+    data() {
+        return {
+            
+        }
+    }
+}
+
+
+</script>
